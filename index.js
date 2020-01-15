a9f0d5b553f2e7425bdb9851b15932d0291809f1
gowa <gowa2017@aliyun.com>
2020-01-15 23:45:30 +0800

    add test out put file

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 #! /usr/bin/env node
-const pandoc=require('pandoc-filter');
+const pandoc = require('pandoc-filter');
 const Viz = require('viz.js');
+const fs = require('fs');
 
-function viz(type, value, format,meta){
+function viz(type, value, format, meta) {
     if (type != "CodeBlock") return null;
-    let [[id,[clazz],[attrs]],content] = value;
+    let [[id, [clazz], [attrs]], content] = value;
     let graphviz_engines = ["circo",
-                            "dot",
-                            "fdp",
-                            "neato",
-                            "osage",
-                            "twopi"];
+        "dot",
+        "fdp",
+        "neato",
+        "osage",
+        "twopi"];
     let isGraphviz = false;
-    for( e of graphviz_engines){
-        if( clazz === e){
+    for (e of graphviz_engines) {
+        if (clazz === e) {
             isGraphviz = true;
             break;
         }
     }
-    if(!isGraphviz) return null;
+    if (!isGraphviz) return null;
     
-    let out = Viz(content,{engine:clazz});
-    let img = "data:image/svg+xml;base64," + new Buffer(out).toString("base64");
-    return pandoc.Para([pandoc.Image([id,[],[]],[pandoc.Str("fig")],[img,""])]);
+
+    let out = Viz(content, { engine: clazz });
+    let img = "data:image/svg+xml;base64," + Buffer.from(out).toString("base64");
+    // console.log(tmpfileObj.name);
+    // fs.writeFileSync('1.svg', out);
+    return pandoc.Para([pandoc.Image([id, [], []], [pandoc.Str("fig")], [img, ""])]);
 }
 
 pandoc.toJSONFilter(viz)
--- test.md
+++ test.md
@@ -5,8 +5,21 @@ a -> b
 }
 ```
 
-```circo
+```dot
 digraph {
-a -> b
+node[fontname="Source Code Pro ExtraLight"]
+A[shape=record,label="longlonglonglonglonglonglonglonglonglonglong"]
+
+}
+
+```
+
+
+```dot
+digraph {
+node[shape=record,style=filled,fillcolor="gray",color="#666"]
+node[fontname="Trebuchet MS"]
+rankdir = LR
+H[label="temp_user_authorize_his | id | <user_id>user_id | parent_id | start_date | end_date",fillcolor="#ECECFF"]
 }
 ```
